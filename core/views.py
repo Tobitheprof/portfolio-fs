e3da14a0dfed430cc20f60a9418be7297594ac05
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-11-30 01:15:42 +0100

    Updated Email Sending Fucntion

--- core/views.py
+++ core/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string, get_template
+from django.core.mail import EmailMessage
+from portfolio import settings
 
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        context = {
+            'name' : name,
+            'email' : email,
+            'subject' : subject,
+            'message' : message,
+        }
+        message = get_template('mail.html').render(context)
+
+        msg = EmailMessage(
+            'Email Received',
+            message,
+            'Oluwatobi Ekundayo',
+            [email],
+        )
+        msg.content_subtype="html"
+        msg.send()
+
+        message2 = get_template('mail2.html').render(context)
+        msg2 = EmailMessage(
+            "Message from your portfolio",
+            message2,
+            'Oluwatobi Ekundayo',
+            [settings.DEFAULT_FROM_EMAIL]
+        )
+        msg2.content_subtype="html"
+        msg2.send()
+
+
+
+
     return render(request, 'index.html')
 
 # Create your views here.
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,11 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 
 import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -127,3 +131,12 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
