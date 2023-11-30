93444e1415ea3515d8b8d6b9df8429ab703a7195
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-11-30 02:57:57 +0100

    Updated Email Send fucntion

--- core/views.py
+++ core/views.py
@@ -1,46 +1,48 @@
 from django.shortcuts import render
-from django.template.loader import render_to_string, get_template
+from django.template.loader import get_template
 from django.core.mail import EmailMessage
 from portfolio import settings
-
+from django.contrib import messages
 
 def index(request):
     if request.method == "POST":
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
 
-        context = {
-            'name' : name,
-            'email' : email,
-            'subject' : subject,
-            'message' : message,
-        }
-        message = get_template('mail.html').render(context)
+        if name and email and subject and message:
+            context = {
+                'name': name,
+                'email': email,
+                'subject': subject,
+                'message': message,
+            }
 
-        msg = EmailMessage(
-            'Email Received',
-            message,
-            'Oluwatobi Ekundayo',
-            [email],
-        )
-        msg.content_subtype="html"
-        msg.send()
+            # Send emails within the same request
+            send_email(context)
 
-        message2 = get_template('mail2.html').render(context)
-        msg2 = EmailMessage(
-            "Message from your portfolio",
-            message2,
-            'Oluwatobi Ekundayo',
-            [settings.DEFAULT_FROM_EMAIL]
-        )
-        msg2.content_subtype="html"
-        msg2.send()
-
-
-
+            messages.success(request, "Hey, I've received your message and I'll respond as soon as I can. While you wait, here are some cool cat videos for you to watch")
 
     return render(request, 'index.html')
 
-# Create your views here.
+def send_email(context):
+    message = get_template('mail.html').render(context)
+    msg = EmailMessage(
+        'Email Received',
+        message,
+        'Oluwatobi Ekundayo',
+        [context['email']],
+    )
+    msg.content_subtype = "html"
+    msg.send()
+
+    message2 = get_template('mail2.html').render(context)
+    msg2 = EmailMessage(
+        "Message from your portfolio",
+        message2,
+        'Oluwatobi Ekundayo',
+        [settings.DEFAULT_FROM_EMAIL]
+    )
+    msg2.content_subtype = "html"
+    msg2.send()
